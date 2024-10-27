bf69eeaaac5b3bac49b936be3410288d640b6fa0
GimenezJorge <gimenezjorgeluis999@gmail.com>
2024-10-27 19:00:52 -0300

    primer version con el carrito de compras

---
+++ TiendaSoftware1/Controllers/CarritoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TiendaSoftware1.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TiendaSoftware1.Controllers
+{
+    public class CarritoController : Controller
+    {
+        private readonly Bdg3Context _context;
+
+        // Lista para almacenar productos en el carrito
+        private static List<Producto> _carrito = new List<Producto>();
+
+        public CarritoController(Bdg3Context context)
+        {
+            _context = context;
+        }
+
+        // Método para agregar un producto al carrito
+        public IActionResult AgregarAlCarrito(int id)
+        {
+            var producto = _context.Productos.FirstOrDefault(p => p.Id == id);
+            if (producto != null)
+            {
+                _carrito.Add(producto);
+            }
+
+            return RedirectToAction("Carrito");
+        }
+
+        // Método para mostrar el carrito
+        public IActionResult Carrito()
+        {
+            // Cambia esto para buscar en la ubicación correcta
+            return View("~/Views/Productos/Carrito.cshtml", _carrito); // Ruta completa a la vista
+        }
+    }
+}
